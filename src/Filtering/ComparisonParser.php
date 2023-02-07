454578f82787b8848398009207fee2a0e794b911
Patrick Morpey <patrick@morphp.nl>
2023-02-07 13:41:42 +0100

    Deserialize query arguments

--- src/Filtering/ComparisonParser.php
+++ src/Filtering/ComparisonParser.php
@@ -4,16 +4,35 @@ declare(strict_types=1);
 
 namespace Medas\RestRequestHandler\Filtering;
 
-use Medas\EntityManager\Selector\Conditions\WhereIs;
-use Medas\EntityManager\Selector\Operants\Property;
-use Medas\EntityManager\Selector\Operants\Value;
+use Medas\EntityManager\MetaDataManager;
+use Medas\EntityManager\Selector\{Conditions\WhereIs, Operants\Property, Operants\Value};
+use Medas\EntityManager\Types\{Guid as GuidType, Relation};
 use Medas\ServiceManager\Attributes\Service;
+use Medas\ServiceManager\Values\Interfaces\GuidProvider;
 
 #[Service]
 class ComparisonParser
 {
+    public function __construct(
+        private readonly MetaDataManager $metaDataManager,
+        private readonly GuidProvider    $guidProvider,
+    )
+    {
+    }
+
     public function parse(QuerySelector $querySelector, string $name, string $value): void
     {
+        $metaData = $this->metaDataManager->get($querySelector->definition()->entity);
+        $type = $metaData->property($name)->type;
+
+        if ($type instanceof Relation) {
+            $type = $this->metaDataManager->get($type->entity)->idProperty->type;
+        }
+
+        if ($type instanceof GuidType) {
+            $value = $this->guidProvider->fromString($value);
+        }
+
         $querySelector->definition()->add(new WhereIs(
             Property::c($name),
             Value::c($value),
