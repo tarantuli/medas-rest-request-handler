d4b6fb4a2154f8dd4c630eebe395ca5b0d4286cf
Patrick Morpey <patrick@morphp.nl>
2023-03-24 13:37:53 +0100

    Added comparison operators

--- src/Filtering/ComparisonParser.php
+++ src/Filtering/ComparisonParser.php
@@ -6,7 +6,17 @@ namespace Medas\RestRequestHandler\Filtering;
 
 use Medas\EntityManager\Exceptions\ClassIsNotAnEntity;
 use Medas\EntityManager\MetaDataManager;
-use Medas\EntityManager\Selector\{Conditions\WhereIs, Operants\Property, Operants\Value};
+use Medas\EntityManager\Selector\{Conditions\WhereContains,
+    Conditions\WhereEndsWith,
+    Conditions\WhereIs,
+    Conditions\WhereIsAtLeast,
+    Conditions\WhereIsAtMost,
+    Conditions\WhereIsLessThan,
+    Conditions\WhereIsMoreThan,
+    Conditions\WhereStartsWith,
+    Operants\Property,
+    Operants\Value
+};
 use Medas\EntityManager\Types\{Guid as GuidType, Relation};
 use Medas\ServiceManager\Attributes\Service;
 use Medas\ServiceManager\Interfaces\GuidProvider;
@@ -23,6 +33,8 @@ class ComparisonParser
 
     public function parse(QuerySelector $querySelector, string $name, string $value): void
     {
+        $comparisonType = $this->getComparisonType($name);
+
         try {
             $metaData = $this->metaDataManager->get($querySelector->definition()->entity);
             $type = $metaData->property($name)->type;
@@ -39,9 +51,51 @@ class ComparisonParser
             // Do nothing
         }
 
-        $querySelector->definition()->add(new WhereIs(
+        $element = new $comparisonType(
             Property::c($name),
             Value::c($value),
-        ));
+        );
+
+        $querySelector->definition()->add($element);
+    }
+
+    private function getComparisonType(&$name): string
+    {
+        if (str_ends_with($name, '<<')) {
+            $name = substr($name, 0, -2);
+            return WhereIsLessThan::class;
+        }
+
+        if (str_ends_with($name, '<')) {
+            $name = substr($name, 0, -1);
+            return WhereIsAtMost::class;
+        }
+
+        if (str_ends_with($name, '>>')) {
+            $name = substr($name, 0, -2);
+            return WhereIsMoreThan::class;
+        }
+
+        if (str_ends_with($name, '>')) {
+            $name = substr($name, 0, -1);
+            return WhereIsAtLeast::class;
+        }
+
+        if (str_ends_with($name, '^')) {
+            $name = substr($name, 0, -1);
+            return WhereStartsWith::class;
+        }
+
+        if (str_ends_with($name, '$')) {
+            $name = substr($name, 0, -1);
+            return WhereEndsWith::class;
+        }
+
+        if (str_ends_with($name, '*')) {
+            $name = substr($name, 0, -1);
+            return WhereContains::class;
+        }
+
+        return WhereIs::class;
     }
 }
