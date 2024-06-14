f6cee7bfd512352cfb0e3b130c5dc6390ed94759
Patrick Morpey <patrick@morphp.nl>
2024-06-14 12:59:49 +0200

    Updated BaseRoutes

--- src/Controllers/BaseRoutes.php
+++ src/Controllers/BaseRoutes.php
@@ -4,30 +4,35 @@ declare(strict_types=1);
 
 namespace Medas\RestRequestHandler\Controllers;
 
-use Medas\RestRequestHandler\Responses\{CollectionResponse, EntityResponse};
+use Medas\EntityManager\Exceptions\PropertyDoesNotExist;
+use Medas\RestRequestHandler\{
+    Exceptions\EntityDoesNotHaveProperty,
+    Filtering\SelectorBuilder,
+    Responses\CollectionResponse,
+    Responses\EntityResponse,
+    Responses\SuccessResponse
+};
 use Medas\Routing\{Methods\Delete, Methods\Get, Methods\Post, Methods\Put, Parameters\Integer};
 
 abstract class BaseRoutes extends BaseController
 {
-    /**
-     * GET /entities
-     */
-    #[Get(isCollectionEndpoint: true)]
-    public function get(): CollectionResponse
-    {
-        return $this->collectionResponseBuilder->build(
-            $this->repository->fetchAll($this->entityClass),
-            $this
-        );
-    }
-
     /**
      * POST /entities
      */
     #[Post]
     public function createEntity(): EntityResponse
     {
-        $entity = em()->create($this->entityClass, $this->bodyData());
+        return $this->createEntityFromData($this->bodyData());
+    }
+
+    protected function createEntityFromData(array $data): EntityResponse
+    {
+        try {
+            $entity = em()->create($this->entityClass, $data);
+        }
+        catch (PropertyDoesNotExist $exception) {
+            throw new EntityDoesNotHaveProperty($this->entityClass, $exception->propertyName);
+        }
 
         em()->persist($entity);
         em()->flush();
@@ -42,6 +47,24 @@ abstract class BaseRoutes extends BaseController
     public function getEntity(int $id): EntityResponse
     {
         return $this->entityResponseBuilder->build(em()->get($this->entityClass, $id), $this);
+    }
+
+    /**
+     * GET /entities
+     */
+    #[Get(isCollectionEndpoint: true)]
+    public function get(): CollectionResponse
+    {
+        if ($queryData = $this->queryData()) {
+            $selectorBuilder = service(SelectorBuilder::class);
+            $selector = $selectorBuilder->build($this->entityClass, $queryData);
+            $entities = $this->repository->fetch($selector);
+        }
+        else {
+            $entities = $this->repository->fetchAll($this->entityClass);
+        }
+
+        return $this->collectionResponseBuilder->build($entities, $this);
     }
 
     /**
@@ -65,13 +88,13 @@ abstract class BaseRoutes extends BaseController
      * DELETE /entities/:id
      */
     #[Delete(new Integer('id'))]
-    public function removeEntity(int $id): bool
+    public function removeEntity(int $id): SuccessResponse
     {
         $entity = em()->get($this->entityClass, $id);
 
         em()->delete($entity);
         em()->flush();
 
-        return true;
+        return new SuccessResponse(true);
     }
 }
