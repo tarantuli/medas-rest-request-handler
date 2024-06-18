52014c2ba8cd7fa80a3abc6f3fa90b3fa23261eb
Patrick Morpey <patrick@morphp.nl>
2024-06-18 13:57:29 +0200

    Added QueryDataSerializer

--- src/Serializers/QueryDataSerializer.php
+++ src/Serializers/QueryDataSerializer.php
@@ -4,12 +4,39 @@ declare(strict_types=1);
 
 namespace Medas\RestRequestHandler\Serializers;
 
-use Medas\Core\{Attributes\Service, Interfaces\Type};
+use Medas\Core\{
+    Attributes\Service,
+    Interfaces\Guid,
+    Interfaces\HasId,
+    Interfaces\Serializer,
+    Interfaces\Type
+};
 use Medas\EntityManager\Types\{Boolean, DateTime, Integer};
 
 #[Service]
-readonly class QueryDataSerializer extends JsonSerializer
+readonly class QueryDataSerializer implements Serializer
 {
+    public function serialize(mixed $value): mixed
+    {
+        if ($value instanceof HasId) {
+            $value = $value->id();
+        }
+
+        if ($value instanceof Guid) {
+            return (string) $value;
+        }
+
+        if (is_bool($value)) {
+            $value = (int) $value;
+        }
+
+        if ($value instanceof \DateTimeInterface) {
+            $value = $value->format(\DateTimeInterface::RFC3339_EXTENDED);
+        }
+
+        return $value;
+    }
+
     public function unserialize(mixed $value, Type $type = null): mixed
     {
         if ($value === 'null') {
