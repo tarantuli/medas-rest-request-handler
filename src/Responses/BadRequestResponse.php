0c1fd062ae2627846816ed9ab8fb0c8149dcb5ee
Patrick Morpey <patrick@morphp.nl>
2024-07-23 12:36:36 +0200

    Improvement

--- src/Responses/BadRequestResponse.php
+++ src/Responses/BadRequestResponse.php
@@ -4,12 +4,19 @@ declare(strict_types=1);
 
 namespace Medas\RestRequestHandler\Responses;
 
-use Medas\HttpRequestHandler\ResponseTypes\HtmlResponse;
+use Medas\HttpRequestHandler\ResponseTypes\{HtmlResponse, JsonResponse};
 
-class BadRequestResponse implements HtmlResponse
+class BadRequestResponse implements HtmlResponse, JsonResponse
 {
     public function outputHtmlResponse(): void
     {
         http_response_code(400);
+    }
+
+    public function getJsonResponse(): null
+    {
+        $this->outputHtmlResponse();
+
+        return null;
     }
 }
--- src/Responses/OptionsResponse.php
+++ src/Responses/OptionsResponse.php
@@ -4,9 +4,9 @@ declare(strict_types=1);
 
 namespace Medas\RestRequestHandler\Responses;
 
-use Medas\HttpRequestHandler\ResponseTypes\HtmlResponse;
+use Medas\HttpRequestHandler\ResponseTypes\{HtmlResponse, JsonResponse};
 
-readonly class OptionsResponse implements HtmlResponse
+readonly class OptionsResponse implements HtmlResponse, JsonResponse
 {
     public function __construct(
         private string      $origin,
@@ -24,5 +24,12 @@ readonly class OptionsResponse implements HtmlResponse
         if ($this->headers !== null) {
             header('Access-Control-Allow-Headers: ' . $this->headers);
         }
+    }
+
+    public function getJsonResponse(): string
+    {
+        $this->outputHtmlResponse();
+
+        return '';
     }
 }
