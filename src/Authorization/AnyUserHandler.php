fbe329680b6e7ffddb92152ec87e89353c4dcedb
Patrick Morpey <patrick@morphp.nl>
2024-07-25 11:54:14 +0200

    Added AnyUser tag

--- src/Authorization/AnyUserHandler.php
+++ src/Authorization/AnyUserHandler.php
@@ -9,11 +9,11 @@ use Medas\HttpRequestHandler\AccessManagement\AuthorizationVote;
 use Medas\Routing\RouteHandler;
 
 /**
- * This authorization vote handlers checks if the route handler method or its declaring class are tagged with PublicResource.
- * If so, then access is allowed. In all other cases, it expresses no opinion.
+ * This authorization vote handlers checks if the route handler method or its declaring class are tagged with AnyUser.
+ * If so, then access is allowed if any user is authenticated. If not, access is disallowed.
  */
 #[Service]
-readonly class PublicResourceHandler
+readonly class AnyUserHandler
 {
     #[EventListener]
     public function handleAuthorizationVote(AuthorizationVote $authorizationVote): void
@@ -24,9 +24,9 @@ readonly class PublicResourceHandler
 
         $methodReflector = $authorizationVote->requestHandler->handlerMethod();
 
-        if (attribute(PublicResource::class, $methodReflector)
-                || attribute(PublicResource::class, $methodReflector->getDeclaringClass())) {
-            $authorizationVote->allowedAccess = true;
+        if (attribute(AnyUser::class, $methodReflector)
+                || attribute(AnyUser::class, $methodReflector->getDeclaringClass())) {
+            $authorizationVote->allowedAccess = $authorizationVote->request->authentication->user !== null;
         }
     }
 }
