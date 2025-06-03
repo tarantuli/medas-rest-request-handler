0eb61795ba54bf406b289c3185de313612869b66
Patrick Morpey <patrick@morphp.nl>
2025-06-03 14:58:08 +0200

    $entityData should be writeable

--- src/Responses/EntityResponse.php
+++ src/Responses/EntityResponse.php
@@ -6,10 +6,10 @@ namespace Medas\RestRequestHandler\Responses;
 
 use Medas\HttpRequestHandler\ResponseTypes\JsonResponse;
 
-readonly class EntityResponse implements JsonResponse
+class EntityResponse implements JsonResponse
 {
     public function __construct(
-        private array $entityData,
+        public array $entityData,
     )
     {
     }
