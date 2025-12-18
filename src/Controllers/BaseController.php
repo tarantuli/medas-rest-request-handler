e278be226e1687c00461d6031e9d23f2a5e9442f
Patrick Morpey <patrick@morphp.nl>
2025-12-18 19:48:14 +0100

    Refactored `BaseController` to simplify `bodyData` handling and deprecated constructor usage in favor of individual Route classes.

--- src/Controllers/BaseController.php
+++ src/Controllers/BaseController.php
@@ -26,9 +26,17 @@ use Medas\Routing\Route;
 
 abstract class BaseController
 {
-    private array $bodyData;
+    private array $bodyData{get{
+
+        if (!isset($this->bodyData)) {
+            $this->bodyData = $this->requestDataHandler->getBodyData($this);
+            } return $this->bodyData;
+        }
+    }
+
     protected string $entityClass;
 
+    #[\Deprecated('create individual Route classes instead')]
     public function __construct(
         protected CollectionResponseBuilder $collectionResponseBuilder,
         protected EntityResponseBuilder     $entityResponseBuilder,
@@ -50,7 +58,7 @@ abstract class BaseController
 
     protected function _createEntity(): EntityResponse
     {
-        return $this->createEntityFromData($this->bodyData());
+        return $this->createEntityFromData($this->bodyData);
     }
 
     protected function createEntityFromData(array $data): EntityResponse
@@ -71,15 +79,6 @@ abstract class BaseController
     protected function _getEntity(mixed $id): EntityResponse
     {
         return $this->entityResponseBuilder->build(em()->get($this->entityClass, $id), $this);
-    }
-
-    protected function bodyData(): array
-    {
-        if (!isset($this->bodyData)) {
-            $this->bodyData = $this->requestDataHandler->getBodyData($this);
-        }
-
-        return $this->bodyData;
     }
 
     protected function _get(): CollectionResponse
@@ -118,7 +117,7 @@ abstract class BaseController
         $entity = em()->get($this->entityClass, $id);
         $metaData = $this->metaDataManager->get($this->entityClass);
 
-        $this->valueSetter->setValues($metaData, $entity, $this->bodyData());
+        $this->valueSetter->setValues($metaData, $entity, $this->bodyData);
 
         em()->persist($entity);
         em()->flush();
