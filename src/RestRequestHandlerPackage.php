f903c872874c9ec749cec6298f4dd8bb8d09eb11
Patrick Morpey <patrick@morphp.nl>
2026-05-11 13:09:34 +0200

    Added `EntityGeneratorPackage` as a dependency in `RestRequestHandlerPackage` to support enhanced entity generation capabilities.

--- src/RestRequestHandlerPackage.php
+++ src/RestRequestHandlerPackage.php
@@ -5,6 +5,7 @@ declare(strict_types=1);
 namespace Medas\RestRequestHandler;
 
 use Medas\Core\{AsSingleton, BasePackage, Interfaces\ServiceConfig};
+use Medas\EntityGenerator\EntityGeneratorPackage;
 use Medas\EntityManager\EntityManagerPackage;
 use Medas\HttpRequestHandler\HttpRequestHandlerPackage;
 use Medas\Routing\RoutingPackage;
@@ -16,6 +17,7 @@ class RestRequestHandlerPackage extends BasePackage
     public function dependencies(): array
     {
         return [
+            EntityGeneratorPackage::instance(),
             EntityManagerPackage::instance(),
             RoutingPackage::instance(),
             HttpRequestHandlerPackage::instance(),
