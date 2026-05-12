43c58828ede7c39251c5b8e468b4300e6ff90584
Patrick Morpey <patrick@morphp.nl>
2026-05-12 09:25:23 +0200

    Refactored `BearerTokenHandler` to offload user resolution logic into `BearerTokenAuthenticator`, introduced `data()` method for streamlined token handling, and updated constructor dependencies to remove unused components and improve maintainability.

---
+++ src/Authentication/BearerTokenAuthenticator.php
@@ -0,0 +1,69 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Medas\RestRequestHandler\Authentication;
+
+use Medas\Core\{
+    Attributes\ConfigValue,
+    Attributes\EventListener,
+    Attributes\Service,
+    Events\DebugInformation,
+    Exceptions\UuidProviderIsNotAvailable,
+    Interfaces\AuthenticationTokenController,
+    Interfaces\Uuid,
+    Interfaces\UuidProvider,
+    Types\Uuid as UuidType
+};
+use Medas\EntityManager\{EntityManager, MetaDataManager};
+use Medas\HttpRequestHandler\Authentication\AuthenticationVote;
+use Medas\RestRequestHandler\ConfigOptions\UsersClass;
+
+#[Service]
+readonly class BearerTokenAuthenticator
+{
+    public function __construct(
+        private AuthenticationTokenController|null $tokenController,
+        private BearerTokenHandler                 $tokenHandler,
+        private EntityManager                      $entityManager,
+        private MetaDataManager                    $metaDataManager,
+        private UuidProvider|null                  $uuidProvider,
+
+        #[ConfigValue(UsersClass::class)]
+        private string|null                        $usersClass,
+    )
+    {
+    }
+
+    #[EventListener]
+    public function validate(AuthenticationVote $vote): void
+    {
+        if ($this->tokenController === null || $this->usersClass === null) {
+            return;
+        }
+
+        if (null === $authData = $this->tokenHandler->data()) {
+            return;
+        }
+
+        $userId = $authData->getUserId();
+        $type = $this->metaDataManager->get($this->usersClass)->idProperty->type;
+
+        if ($userId !== null && $type instanceof UuidType && !($userId instanceof Uuid)) {
+            if ($this->uuidProvider === null) {
+                throw new UuidProviderIsNotAvailable();
+            }
+
+            $userId = $this->uuidProvider->fromString($userId);
+        }
+
+        if ($userId) {
+            dispatch(new DebugInformation('[bearer-token] found user %s', $userId));
+
+            $vote->user = $this->entityManager->get($this->usersClass, $userId);
+        }
+        else {
+            dispatch(new DebugInformation('[bearer-token] no user found with id "%s"', $userId));
+        }
+    }
+}
--- src/Authentication/BearerTokenHandler.php
+++ src/Authentication/BearerTokenHandler.php
@@ -5,76 +5,49 @@ declare(strict_types=1);
 namespace Medas\RestRequestHandler\Authentication;
 
 use Medas\Core\{
-    Attributes\ConfigValue,
-    Attributes\EventListener,
     Attributes\Service,
     Events\DebugInformation,
-    Exceptions\UuidProviderIsNotAvailable,
-    Interfaces\AuthenticationTokenController,
-    Interfaces\Uuid,
-    Interfaces\UuidProvider,
-    Types\Uuid as UuidType
+    Interfaces\AuthenticationData,
+    Interfaces\AuthenticationTokenController
 };
-use Medas\EntityManager\{EntityManager, MetaDataManager};
-use Medas\HttpRequestHandler\{Authentication\AuthenticationVote, Request\HeaderFinder};
-use Medas\RestRequestHandler\ConfigOptions\UsersClass;
+use Medas\HttpRequestHandler\{Request\HeaderFinder, RequestFactory};
 
 #[Service]
 readonly class BearerTokenHandler
 {
     public function __construct(
         private AuthenticationTokenController|null $tokenController,
-        private EntityManager                      $entityManager,
         private HeaderFinder                       $headerFinder,
-        private MetaDataManager                    $metaDataManager,
-        private UuidProvider|null                  $uuidProvider,
-
-        #[ConfigValue(UsersClass::class)]
-        private string|null                        $usersClass,
+        private RequestFactory                     $requestFactory,
     )
     {
     }
 
-    #[EventListener]
-    public function validate(AuthenticationVote $vote): void
+    public function data(): AuthenticationData|null
     {
-        if ($this->tokenController === null || $this->usersClass === null) {
-            return;
+        if ($this->tokenController === null) {
+            return null;
         }
 
-        $header = $this->headerFinder->find($vote->request->serverData, 'Authorization');
+        $header = $this->headerFinder->find(
+            $this->requestFactory->get()->serverData,
+            'Authorization'
+        );
 
         if ($header === null) {
             dispatch(new DebugInformation('[bearer-token] Authorization header not found'));
 
-            return;
+            return null;
         }
 
         if (!str_starts_with($header, 'Bearer ')) {
             dispatch(new DebugInformation('[bearer-token] Authorization header does not start with "Bearer"'));
 
-            return;
+            return null;
         }
 
         $token = substr($header, 7);
-        $userId = $this->tokenController->userId($token);
-        $type = $this->metaDataManager->get($this->usersClass)->idProperty->type;
 
-        if ($userId !== null && $type instanceof UuidType && !($userId instanceof Uuid)) {
-            if ($this->uuidProvider === null) {
-                throw new UuidProviderIsNotAvailable();
-            }
-
-            $userId = $this->uuidProvider->fromString($userId);
-        }
-
-        if ($userId) {
-            dispatch(new DebugInformation('[bearer-token] found user %s', $userId));
-
-            $vote->user = $this->entityManager->get($this->usersClass, $userId);
-        }
-        else {
-            dispatch(new DebugInformation('[bearer-token] no user found with id "%s"', $userId));
-        }
+        return $this->tokenController->data($token);
     }
 }
